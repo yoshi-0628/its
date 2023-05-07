047f01ecff82c751d839230beecd3115209280e1
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2023-05-07 14:46:41 +0900

    TODOアプリケーション完成

---
+++ src/main/java/com/example/its/web/issue/IssueForm.java
@@ -0,0 +1,15 @@
+package com.example.its.web.issue;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class IssueForm {
+    @NotBlank
+    @Size(max=256)
+    private String summary;
+    @NotBlank
+    @Size(max=256)
+    private String description;
+}
